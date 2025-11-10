2c2407d30ee0c8bc5025fbd88e4f201625258480
Leonardo Pio Francesco Gallina <s349363@studenti.polito.it>
2025-11-10 16:21:53 +0100

    add: RTL encoder/decoder 1 cc

---
+++ src/1_cycle_tot/secded_encoder.sv
@@ -0,0 +1,61 @@
+module secded_encoder (
+  input  logic        CLK,
+  input  logic [63:0] R_DATA,
+  output logic [71:0] E_DATA
+);
+
+  logic [71:0] BUFFER;
+
+  assign E_DATA = BUFFER;
+
+  always @(posedge CLK) begin
+    BUFFER [63:0] <= R_DATA;
+  
+    // C1
+    BUFFER[64] <= R_DATA[0] ^ R_DATA[1] ^ R_DATA[2] ^ R_DATA[3] ^ R_DATA[4] ^ R_DATA[5] ^ R_DATA[6] ^
+      R_DATA[7] ^ R_DATA[10] ^ R_DATA[13] ^ R_DATA[14] ^ R_DATA[17] ^ R_DATA[20] ^ R_DATA[23] ^
+		  R_DATA[24] ^ R_DATA[27] ^ R_DATA[35] ^ R_DATA[43] ^ R_DATA[46] ^ R_DATA[47] ^ R_DATA[51] ^
+		  R_DATA[52] ^ R_DATA[53] ^ R_DATA[56] ^ R_DATA[57] ^ R_DATA[58];
+    // C2
+    BUFFER[65] <= R_DATA[0] ^ R_DATA[1] ^ R_DATA[2] ^ R_DATA[8] ^ R_DATA[9] ^ R_DATA[10] ^ R_DATA[11] ^
+  	  R_DATA[12] ^ R_DATA[13] ^ R_DATA[14] ^ R_DATA[15] ^ R_DATA[18] ^ R_DATA[21] ^ R_DATA[22] ^
+  		R_DATA[25] ^ R_DATA[28] ^ R_DATA[31] ^ R_DATA[32] ^ R_DATA[35] ^ R_DATA[43] ^ R_DATA[51] ^
+  	  R_DATA[54] ^ R_DATA[55] ^ R_DATA[59] ^ R_DATA[60] ^ R_DATA[61];
+  
+    // C3
+    BUFFER[66] <= R_DATA[3] ^ R_DATA[4] ^ R_DATA[5] ^ R_DATA[8] ^ R_DATA[9] ^ R_DATA[10] ^ R_DATA[16] ^
+  	  R_DATA[17] ^ R_DATA[18] ^ R_DATA[19] ^ R_DATA[20] ^ R_DATA[21] ^ R_DATA[22] ^ R_DATA[23] ^
+  		R_DATA[26] ^ R_DATA[29] ^ R_DATA[30] ^ R_DATA[33] ^ R_DATA[36] ^ R_DATA[39] ^ R_DATA[40] ^
+  	  R_DATA[43] ^ R_DATA[51] ^ R_DATA[59] ^ R_DATA[62] ^ R_DATA[63];
+  
+    // C4
+    BUFFER[67] <= R_DATA[3] ^ R_DATA[6] ^ R_DATA[7] ^ R_DATA[11] ^ R_DATA[12] ^ R_DATA[13] ^ R_DATA[16] ^ 
+  		R_DATA[17] ^ R_DATA[18] ^ R_DATA[24] ^ R_DATA[25] ^ R_DATA[26] ^ R_DATA[27] ^ R_DATA[28] ^ 
+  	  R_DATA[29] ^ R_DATA[30] ^ R_DATA[31] ^ R_DATA[34] ^ R_DATA[37] ^ R_DATA[38] ^ R_DATA[41] ^ 
+  	  R_DATA[44] ^ R_DATA[47] ^ R_DATA[48] ^ R_DATA[51] ^ R_DATA[59];
+  
+    // C5
+    BUFFER[68] <= R_DATA[3] ^ R_DATA[11] ^ R_DATA[14] ^ R_DATA[15] ^ R_DATA[19] ^ R_DATA[20] ^ R_DATA[21] ^
+  	  R_DATA[24] ^ R_DATA[25] ^ R_DATA[26] ^ R_DATA[32] ^ R_DATA[33] ^ R_DATA[34] ^ R_DATA[35] ^
+  		R_DATA[36] ^ R_DATA[37] ^ R_DATA[38] ^ R_DATA[39] ^ R_DATA[42] ^ R_DATA[45] ^ R_DATA[46] ^
+  	  R_DATA[49] ^ R_DATA[52] ^ R_DATA[55] ^ R_DATA[56] ^ R_DATA[59];
+  
+    // C6
+    BUFFER[69] <= R_DATA[0] ^ R_DATA[3] ^ R_DATA[11] ^ R_DATA[19] ^ R_DATA[22] ^ R_DATA[23] ^ R_DATA[27] ^
+  	  R_DATA[28] ^ R_DATA[29] ^ R_DATA[32] ^ R_DATA[33] ^ R_DATA[34] ^ R_DATA[40] ^ R_DATA[41] ^
+  		R_DATA[42] ^ R_DATA[43] ^ R_DATA[44] ^ R_DATA[45] ^ R_DATA[46] ^ R_DATA[47] ^ R_DATA[50] ^
+  		R_DATA[53] ^ R_DATA[54] ^ R_DATA[57] ^ R_DATA[60] ^ R_DATA[63];
+
+    // C7
+    BUFFER[70] <= R_DATA[1] ^ R_DATA[4] ^ R_DATA[7] ^ R_DATA[8] ^ R_DATA[11] ^ R_DATA[19] ^ R_DATA[27] ^
+      R_DATA[30] ^ R_DATA[31] ^ R_DATA[35] ^ R_DATA[36] ^ R_DATA[37] ^ R_DATA[40] ^ R_DATA[41] ^
+      R_DATA[42] ^ R_DATA[48] ^ R_DATA[49] ^ R_DATA[50] ^ R_DATA[51] ^ R_DATA[52] ^ R_DATA[53] ^
+      R_DATA[54] ^ R_DATA[55] ^ R_DATA[58] ^ R_DATA[61] ^ R_DATA[62];
+
+    // C8
+    BUFFER[71] <= R_DATA[2] ^ R_DATA[5] ^ R_DATA[6] ^ R_DATA[9] ^ R_DATA[12] ^ R_DATA[15] ^ R_DATA[16] ^
+      R_DATA[19] ^ R_DATA[27] ^ R_DATA[35] ^ R_DATA[38] ^ R_DATA[39] ^ R_DATA[43] ^ R_DATA[44] ^
+     	R_DATA[45] ^ R_DATA[48] ^ R_DATA[49] ^ R_DATA[50] ^ R_DATA[56] ^ R_DATA[57] ^ R_DATA[58] ^
+      R_DATA[59] ^ R_DATA[60] ^ R_DATA[61] ^ R_DATA[62] ^ R_DATA[63];
+  end 
+endmodule
---
+++ src/1_cycle_tot/testbench.sv
@@ -0,0 +1,76 @@
+module testbench;
+  logic CLK;
+  logic [63:0] R_DATA;
+  logic [71:0] E_DATA;
+  logic [71:0] D_DATA;
+  logic        ERR;
+  logic        D_ERR;
+  logic        S_ERR;
+  logic [71:0] N_DATA;
+  logic [71:0] NOISE;
+
+  string dumpfile_name;
+
+  secded_encoder dut_encoder (
+    .CLK (CLK),
+    .R_DATA (R_DATA),
+    .E_DATA (E_DATA)
+  );
+
+  secded_decoder dut_decoder (
+    .CLK (CLK),
+    .E_DATA (N_DATA),
+    .D_DATA (D_DATA),
+    .ERR (ERR),
+    .D_ERR (D_ERR),
+    .S_ERR (S_ERR)
+  );
+
+  initial begin
+    CLK = 1'b0;
+    forever #5 CLK = ~CLK;
+  end
+
+  initial begin
+    $monitor("time=%3d, data=%h, encoded=%h, decoded=%h, error_detected=%b, double_error=%b, single_error=%b", $time, R_DATA, E_DATA, D_DATA, ERR, D_ERR, S_ERR);
+    #2
+    R_DATA = 64'hDEAD_BEEF_CAFE_CAFE;
+    #10
+    R_DATA = 64'hCAFE_CAFE_DEAD_BEEF;
+    #10
+    R_DATA = 64'h1212_3434_5656_7878;
+    #10
+    R_DATA = 64'hDEAD_BEEF_CAFE_CAFE;
+    #10
+    R_DATA = 64'hCAFE_CAFE_DEAD_BEEF;
+    #10
+    R_DATA = 64'h1212_3434_5656_7878;
+    #10
+    R_DATA = 64'hDEAD_BEEF_CAFE_CAFE;
+    #10
+    R_DATA = 64'hCAFE_CAFE_DEAD_BEEF;
+    #10
+    R_DATA = 64'h1212_3434_5656_7878;
+    #50
+    $finish;
+  end
+
+  initial begin
+    NOISE = 72'h00_0000_0000_0000_0000; // NO ERR
+    #37
+    NOISE = 72'h00_0000_0000_0010_0000; // SEC
+    #30
+    NOISE = 72'h00_0110_0000_0000_0000; // DED
+  end
+
+  assign N_DATA = E_DATA ^ NOISE;
+
+  initial begin
+    if ($value$plusargs("dumpfile=%s", dumpfile_name))
+      $dumpfile(dumpfile_name);
+    else
+      $dumpfile("generic_waveform.vcd");
+    $dumpvars(0, testbench);
+  end
+endmodule
+
